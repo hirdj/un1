b24392a7954edbbe91179daa6dc51787d5fe5586
hirdj <shrirangsonawane8@gmail.com>
2024-05-08 00:16:03 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -26,35 +26,35 @@
 
 
 
-from collections import deque
+# from collections import deque
 
-def bfs(graph, start):
-    visited = set()
-    traversal_order = []
-    queue = deque([start])
+# def bfs(graph, start):
+#     visited = set()
+#     traversal_order = []
+#     queue = deque([start])
 
-    while queue:
-        node = queue.popleft()
-        if node not in visited:
-            visited.add(node)
-            traversal_order.append(node)
-            for neighbor in graph[node]:
-                if neighbor not in visited:
-                    queue.append(neighbor)
+#     while queue:
+#         node = queue.popleft()
+#         if node not in visited:
+#             visited.add(node)
+#             traversal_order.append(node)
+#             for neighbor in graph[node]:
+#                 if neighbor not in visited:
+#                     queue.append(neighbor)
 
-    return traversal_order
+#     return traversal_order
 
-# Example usage:
-graph = {
-    'A': ['B', 'C'],
-    'B': ['D', 'E'],
-    'C': ['F'],
-    'D': [],
-    'E': ['F'],
-    'F': []
-}
+# # Example usage:
+# graph = {
+#     'A': ['B', 'C'],
+#     'B': ['D', 'E'],
+#     'C': ['F'],
+#     'D': [],
+#     'E': ['F'],
+#     'F': []
+# }
 
-print("Breadth-First Traversal:", bfs(graph, 'A'))
+# print("Breadth-First Traversal:", bfs(graph, 'A'))
 
 
 
